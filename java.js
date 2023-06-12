295d12c9aecbf725ef004d7484b99806929e621a
Simon-Eriksson <127835440+Simon-Eriksson@users.noreply.github.com>
2023-06-12 11:07:32 +0200

    new start

--- java.js
+++ java.js
@@ -1,36 +1,43 @@
-const notificationElement = document.querySelector(".notification")
-const iconElement = document.querySelector(".weather-icon")
-const tempElement = document.querySelector(".tempratur-value p")
-const descElement = document.querySelector(".temperature-description p")
-const locationElement = document.querySelector(".location p")
+window.addEventListener("load", ()=> {
+    let long;
+    let lat;
 
-//const weather = {
+    if (navigator.geolocation)
+})
+
+//const notificationElement = document.querySelector(".notification")
+//const iconElement = document.querySelector(".weather-icon")
+//const tempElement = document.querySelector(".tempratur-value p")
+//const descElement = document.querySelector(".temperature-description p")
+//const locationElement = document.querySelector(".location p")
 //
-//    temperature : {
-//        value : 18,
-//        unit : "celcius"
-//    },
+////const weather = {
+////
+////    temperature : {
+////        value : 18,
+////        unit : "celcius"
+////    },
+////
+////    description : "few clouds",
+////    iconId : "01d",
+////    city : "London",
+////    country : "GB"
+////}
+////
 //
-//    description : "few clouds",
-//    iconId : "01d",
-//    city : "London",
-//    country : "GB"
-//}
+//iconElement.innerHTML = 
+//`<img scr="icons/${weather.iconId}.png"/>`;
 //
-
-iconElement.innerHTML = 
-`<img scr="icons/${weather.iconId}.png"/>`;
-
-tempElement.innerHTML = 
-`${weather.temperature.value} ° <span>C</span>`
-
-descElement.innerHTML = 
-
-locationElement.innerHTML = 
-
-
-//fetch ("https://api.openweathermap.org/data/2.5/weather?q=KALMAR&appid=327c6ab2c31e9fa34141ecb5115d3ccb")
-//.then(response => response.json())
-//.then (data => console.log(data))
+//tempElement.innerHTML = 
+//`${weather.temperature.value} ° <span>C</span>`
 //
-//.catch(err => alert("wrong city name!"))
+//descElement.innerHTML = 
+//
+//locationElement.innerHTML = 
+//
+//
+////fetch ("https://api.openweathermap.org/data/2.5/weather?q=KALMAR&appid=327c6ab2c31e9fa34141ecb5115d3ccb")
+////.then(response => response.json())
+////.then (data => console.log(data))
+////
+////.catch(err => alert("wrong city name!"))
