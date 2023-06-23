ea450fe4484faa9eb56ab0a9549f6356af7c36c3
Simon-Eriksson <127835440+Simon-Eriksson@users.noreply.github.com>
2023-06-23 08:37:42 +0200

    Done

--- weather-app.js
+++ weather-app.js
@@ -1,44 +1,42 @@
-let weather = {
-    "apiKey": "327c6ab2c31e9fa34141ecb5115d3ccb",
-    fetchWeather: function (city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="
-             + city 
-             + "&units=metric&appid=" 
-             + this.apiKey
-             )
-        .then ((response) => response.json ())
-        .then ((data) => this.displayWeather(data))
-    },
-    displayWeather: function(data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        const { timezone } = data.timezone;
-        const { date } = data.dt;
-        console.log(name,icon,description,temp,humidity,speed,timezone);
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
-        getDate(date,timezone)
-    }
-};
+function weather() {
+    var apiKey = "327c6ab2c31e9fa34141ecb5115d3ccb";
+    fetchWeather(apiKey);
 
-function getDate(dt, timezone) {
-    const utc_seconds = parseInt(dt, 10) + parseInt(timezone, 10);
-    const utc_milliseconds = utc_seconds * 1000;
-    const local_date = new Date(utc_milliseconds).toUTCString();
-    console.log(local_date);
 }
 
+function fetchWeather(key) {
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+         + "kalmar" 
+         + "&units=metric&appid=" 
+         + key
+         )
+    .then ((response) => response.json ())
+    .then ((data) => displayWeather(data))
+}
 
-//Ett huvudvärde som beskriver vädret (e.g. regn, moln, sol)
-//LÄGG IN EN GEOCODE FÖR FÖRSTA VÄRDET NÄR MAN ÖPPNAR HEMSIDAN.
-//Värden för temperatur, lufttryck, fuktighet och vind
-//Datum och tid (e.g. 2023-07-20 13:23)
-//Datum och tid ska uppdateras minst en gång per sekund och alla andra värden ska uppdateras minst var 30:e minut.
-//Allt ska visas upp genom HTML & CSS med JavaScript som styr alla värden som kan uppdateras. Det finns inga designkrav.
+function displayWeather (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+    console.log("weather");
+}
+
+function calcTime() {
+    var d = new Date().toLocaleString("en-GB", {timeZone: "CET"})
+    document.querySelector(".time").innerText = d;
+    console.log("time");
+}
+
+weather();
+calcTime();
+
+setInterval (calcTime, 1000);
+setInterval (weather, 1800000);
